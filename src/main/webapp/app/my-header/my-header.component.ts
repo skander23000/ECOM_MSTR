f21a32459f567c626ec4e4eae134d7e25fbc095d
romb38 <r.barbier38@gmail.com>
2024-09-09 13:16:55 +0200

    Add MyHeaderComponent

---
+++ src/main/webapp/app/my-header/my-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-my-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-header.component.html',
+  styleUrl: './my-header.component.scss',
+})
+export class MyHeaderComponent {}
