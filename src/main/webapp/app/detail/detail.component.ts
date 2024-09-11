848830d409a449fbfb96c324389984e66e26e007
745th <theolorgnier@outlook.fr>
2024-09-11 17:03:58 +0200

    theo/cart rebase main 2.0

--- src/main/webapp/app/detail/detail.component.ts
+++ src/main/webapp/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ITire } from 'app/entities/tire/tire.model';
+import { BasketService } from 'app/basket.service';
 
 @Component({
   selector: 'jhi-detail',
@@ -11,16 +12,26 @@ import { ITire } from 'app/entities/tire/tire.model';
 export class DetailComponent {
   @Input() tire: ITire | null = null;
   @Output() closeModal = new EventEmitter<void>();
-  quantity = 1;
+  quantity = 0;
+  constructor(private basket: BasketService) {}
 
-  decreaseQuantity(): void {
-    if (this.quantity > 1) {
-      this.quantity--;
+  removeATire(tire: ITire | null): void {
+    if (!tire) {
+      return;
     }
+    this.basket.removeTires(tire);
   }
-
-  increaseQuantity(): void {
-    this.quantity++;
+  addTire(tire: ITire | null): void {
+    if (!tire) {
+      return;
+    }
+    this.basket.addTire(tire);
+  }
+  setTire(tire: ITire | null, count: number): void {
+    if (!tire) {
+      return;
+    }
+    this.basket.setTire(tire, count);
   }
   close(): any {
     this.closeModal.emit();
