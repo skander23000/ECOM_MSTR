abdbadd51fc9e1ddda899d8012591ea31e0385bd
745th <theolorgnier@outlook.fr>
2024-09-13 13:13:41 +0200

    Add Observable support

--- src/main/webapp/app/basket.service.ts
+++ src/main/webapp/app/basket.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { ITire } from './entities/tire/tire.model';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { ApplicationConfigService } from './core/config/application-config.service';
+import { createRequestOption } from './core/request/request-util';
+import { Observable } from 'rxjs';
 
 interface TireContainer {
   tire: ITire;
+  count: number;
+}
+
+interface IDContainer {
+  id: number;
   count: number;
 }
 
@@ -10,44 +19,67 @@ interface TireContainer {
   providedIn: 'root',
 })
 export class BasketService {
-  setTire(t_tire: ITire, t_count: number): void {
-    let basket = this.getContent();
-    // eslint-disable-next-line no-console
-    console.log(t_count);
-    if (basket.length > 0) {
-      const item: any = basket.find(x => x.tire.id === t_tire.id);
-      // eslint-disable-next-line no-console
-      console.log(item);
-      if (item !== undefined) {
-        const index: number = basket.indexOf(item);
-        basket[index].count = t_count;
-      } else {
-        basket.push({ count: t_count, tire: t_tire });
-      }
-    } else {
-      basket = new Array(1);
-      basket[0] = { count: 1, tire: t_tire };
-    }
-    localStorage.setItem('basket', JSON.stringify(basket));
-  }
+  private http = inject(HttpClient);
+  private applicationConfigService = inject(ApplicationConfigService);
+  private resourceUrl = this.applicationConfigService.getEndpointFor('api/item-list-locks');
 
-  addTire(t_tire: ITire): void {
-    let basket = this.getContent();
-    if (basket.length > 0) {
-      const item: any = basket.find(x => x.tire.id === t_tire.id);
+  addTire(t_tire: ITire, t_count = 1): Observable<boolean> {
+    // eslint-disable-next-line no-console
+    console.log('hey1');
+    return new Observable<boolean>(sub => {
       // eslint-disable-next-line no-console
-      console.log(item);
-      if (item !== undefined) {
-        const index: number = basket.indexOf(item);
-        basket[index].count += 1;
-      } else {
-        basket.push({ count: 1, tire: t_tire });
-      }
-    } else {
-      basket = new Array(1);
-      basket[0] = { count: 1, tire: t_tire };
-    }
-    localStorage.setItem('basket', JSON.stringify(basket));
+      console.log('hey11');
+      this.checkAccountValidity().subscribe({
+        next: value => {
+          let nbtire = 0;
+          let basket = this.getContent();
+          if (basket.length > 0) {
+            // eslint-disable-next-line no-console
+            console.log('hey2');
+            const item: any = basket.find(x => x.tire.id === t_tire.id);
+            if (item !== undefined) {
+              const index: number = basket.indexOf(item);
+              basket[index].count += t_count;
+              nbtire = basket[index].count;
+              // eslint-disable-next-line no-console
+              console.log('hey3');
+            } else {
+              basket.push({ count: t_count, tire: t_tire });
+              nbtire = t_count;
+              // eslint-disable-next-line no-console
+              console.log('hey4');
+            }
+          } else {
+            basket = new Array(1);
+            basket[0] = { count: t_count, tire: t_tire };
+            nbtire = t_count;
+            // eslint-disable-next-line no-console
+            console.log('hey5');
+          }
+          this.setTireBDD(t_tire, nbtire).subscribe({
+            next(value2) {
+              sub.next(true);
+              sub.complete();
+              localStorage.setItem('basket', JSON.stringify(basket));
+              // eslint-disable-next-line no-console
+              console.log('hey6');
+            },
+            error(err) {
+              // eslint-disable-next-line no-console
+              console.log('hey7');
+              sub.error('101|No Enough Items !');
+            },
+          });
+        },
+        error(err) {
+          sub.error('102|Account timeout !');
+        },
+        complete() {
+          // eslint-disable-next-line no-console
+          console.log('heyo');
+        },
+      });
+    });
   }
 
   getContent(): TireContainer[] {
@@ -56,30 +88,73 @@ export class BasketService {
     return result;
   }
 
-  removeATire(t_tire: ITire): void {
-    const basket = this.getContent();
-    if (basket.length > 0) {
-      const item: any = basket.find(x => x.tire.id === t_tire.id);
-      // eslint-disable-next-line no-console
-      console.log(item);
-      if (item !== undefined) {
-        const index: number = basket.indexOf(item);
-        if (basket[index].count > 1) basket[index].count -= 1;
-      }
-    }
-    localStorage.setItem('basket', JSON.stringify(basket));
+  refreshContent(): Observable<HttpResponse<TireContainer>> {
+    const options = createRequestOption('test');
+    return this.http.get<TireContainer>(`${this.resourceUrl}/check-availability`, { params: options, observe: 'response' });
   }
 
-  removeTires(t_tire: ITire): void {
-    let basket: TireContainer[] = this.getContent();
-    if (basket.find(p => p.tire.id === t_tire.id)) {
-      basket = basket.filter(p => p.tire.id !== t_tire.id);
-      localStorage.setItem('basket', JSON.stringify(basket));
-    } else {
-      throw new Error('Item has to exist !');
-    }
+  removeATire(t_tire: ITire): Observable<boolean> {
+    return new Observable<boolean>(sub => {
+      this.checkAccountValidity().subscribe({
+        next: value => {
+          const basket = this.getContent();
+          let nbtire = 0;
+          if (basket.length > 0) {
+            const item: any = basket.find(x => x.tire.id === t_tire.id);
+            if (item !== undefined) {
+              const index: number = basket.indexOf(item);
+              if (basket[index].count > 1) {
+                basket[index].count -= 1;
+              } else {
+                sub.error("104|can't add -1 tire!");
+              }
+              nbtire = basket[index].count;
+            }
+            this.setTireBDD(t_tire, nbtire).subscribe({
+              next(value2) {
+                sub.next(true);
+                sub.complete();
+                localStorage.setItem('basket', JSON.stringify(basket));
+              },
+              error(err) {
+                sub.error("104|can't add -1 tire!");
+              },
+            });
+          }
+        },
+        error(err) {
+          sub.error('102|Account timeout !');
+        },
+      });
+    });
   }
-  getNumberOfTires(t_tire: ITire): number {
+
+  removeTires(t_tire: ITire): Observable<any> {
+    return new Observable<boolean>(sub => {
+      this.checkAccountValidity().subscribe({
+        next: value => {
+          let basket: TireContainer[] = this.getContent();
+          if (basket.find(p => p.tire.id === t_tire.id)) {
+            basket = basket.filter(p => p.tire.id !== t_tire.id);
+            this.setTireBDD(t_tire, 0).subscribe({
+              next(value2) {
+                localStorage.setItem('basket', JSON.stringify(basket));
+              },
+              error(err) {
+                sub.error('103|Item has to exist !');
+              },
+            });
+          } else {
+            sub.error('100|Item has to exist !');
+          }
+        },
+        error(err) {
+          sub.error('102|Account timeout !');
+        },
+      });
+    });
+  }
+  getNumberOfATire(t_tire: ITire): number {
     const basket: TireContainer[] = this.getContent();
     if (basket.length > 0) {
       const item: any = basket.find(x => x.tire === t_tire);
@@ -87,12 +162,90 @@ export class BasketService {
         const index: number = basket.indexOf(item);
         return basket[index].count;
       }
-      return 0;
-    } else {
-      throw new Error('Item has to exist !');
     }
+    return 0;
   }
-  wipe(): void {
-    localStorage.setItem('basket', JSON.stringify('{}'));
+
+  getNumberOfTires(): number {
+    const basket: TireContainer[] = this.getContent();
+    if (!Array.isArray(basket)) return 0;
+    return basket.length;
+  }
+
+  wipe(): Observable<boolean> {
+    return new Observable<boolean>(sub => {
+      this.checkAccountValidity().subscribe({
+        next: value => {
+          const basket = this.getContent();
+          for (const tire of basket) {
+            this.removeTires(tire.tire).subscribe();
+
+            localStorage.setItem('basket', JSON.stringify('{}'));
+            sub.next(true);
+            sub.complete();
+          }
+        },
+        error(err) {
+          sub.error('102|Account timeout !');
+        },
+      });
+    });
+  }
+
+  checkAccountValidity(): Observable<HttpResponse<boolean>> {
+    const options = createRequestOption('test');
+    return new Observable<HttpResponse<boolean>>(hey => {
+      hey.next(new HttpResponse<boolean>());
+      hey.complete();
+    });
+    // return this.http.get<boolean>(`${this.resourceUrl}/check-availability`, { params: options, observe: 'response' });
+  }
+  setTire(t_tire: ITire, t_count: number): Observable<boolean> {
+    return new Observable<boolean>(sub => {
+      this.checkAccountValidity().subscribe({
+        next: value => {
+          let basket = this.getContent();
+          if (t_count <= 0) {
+            sub.error("104|can't add -1 tire!");
+          } else {
+            if (basket.length > 0) {
+              const item: any = basket.find(x => x.tire.id === t_tire.id);
+              if (item !== undefined) {
+                const index: number = basket.indexOf(item);
+                basket[index].count = t_count;
+              } else {
+                basket.push({ count: t_count, tire: t_tire });
+              }
+            } else {
+              basket = new Array(1);
+              basket[0] = { count: t_count, tire: t_tire };
+            }
+            this.setTireBDD(t_tire, t_count).subscribe({
+              next(value2) {
+                localStorage.setItem('basket', JSON.stringify(basket));
+                sub.next(true);
+                sub.complete();
+              },
+              error(err) {
+                sub.error('101|No Enough Items !');
+              },
+            });
+          }
+        },
+        error(err) {
+          sub.error('102|Account timeout !');
+        },
+      });
+    });
+  }
+
+  private setTireBDD(t_tire: ITire, t_count: number): Observable<void> {
+    const container: IDContainer = { id: t_tire.id, count: t_count };
+    const options = createRequestOption(container);
+    return new Observable(hey => {
+      hey.next();
+      hey.complete();
+    });
+    // return this.http.get<boolean>(`${this.resourceUrl}/check-availability`, { params: options, observe: 'response' });
   }
 }
--- src/main/webapp/app/detail/detail.component.ts
+++ src/main/webapp/app/detail/detail.component.ts
@@ -1,37 +1,67 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ITire } from 'app/entities/tire/tire.model';
-import { BasketService } from 'app/basket.service';
+import { BasketService } from '../basket.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'jhi-detail',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
   @Input() tire: ITire | null = null;
   @Output() closeModal = new EventEmitter<void>();
-  quantity = 0;
+  quantity = 1;
+
   constructor(private basket: BasketService) {}
 
-  removeATire(tire: ITire | null): void {
-    if (!tire) {
-      return;
+  decreaseQuantity(tire: ITire | null): void {
+    if (this.quantity > 1) {
+      this.quantity--;
     }
-    this.basket.removeTires(tire);
+    if (tire)
+      this.basket.removeATire(tire).subscribe({
+        next(value) {
+          // eslint-disable-next-line no-console
+          console.log('Success');
+        },
+        error(err) {
+          // eslint-disable-next-line no-console
+          console.log('Failed');
+        },
+      });
   }
-  addTire(tire: ITire | null): void {
-    if (!tire) {
-      return;
+
+  setvalue(tire: ITire | null): void {
+    if (Number(this.quantity) && tire) {
+      this.basket.setTire(tire, Number(this.quantity)).subscribe({
+        next(value) {
+          // eslint-disable-next-line no-console
+          console.log('__');
+        },
+        error(err) {
+          // eslint-disable-next-line no-console
+          console.log('__');
+        },
+      });
     }
-    this.basket.addTire(tire);
   }
-  setTire(tire: ITire | null, count: number): void {
-    if (!tire) {
-      return;
-    }
-    this.basket.setTire(tire, count);
+
+  increaseQuantity(tire: ITire | null): void {
+    this.quantity++;
+    if (tire)
+      this.basket.addTire(tire).subscribe({
+        next(value) {
+          // eslint-disable-next-line no-console
+          console.log('__');
+        },
+        error(err) {
+          // eslint-disable-next-line no-console
+          console.log('__');
+        },
+      });
   }
   close(): any {
     this.closeModal.emit();
